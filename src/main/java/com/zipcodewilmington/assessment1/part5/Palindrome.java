9772ba61aabb1fb1b53b17c93f1c9e83238d4587
donnasvinis <dvsvinis@mac.com>
2019-06-03 16:35:46 -0400

    Part5 Started to try to solve

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,6 +3,25 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        Integer count = 0;
+        String reversed = "";
+        int start = 0;
+        int end = input.length()-1;
+
+ //      while (start < input.length/2){
+
+ //       for (start; end; )
+            //create reverse
+            String reverse = "";
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed = reversed + input.charAt(i);
+            }
+            //compare
+            if (input == reversed) {
+                count++;
+            }
+
+ //       }
+        return count;
     }
 }
