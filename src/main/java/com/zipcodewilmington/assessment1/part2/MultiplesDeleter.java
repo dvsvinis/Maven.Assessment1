578f8024d3f2c0d1c322b544af20bc58516ea67b
donnasvinis <dvsvinis@mac.com>
2019-06-03 13:55:16 -0400

    Part2 StringUtils test1

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -10,8 +12,18 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with evens removed
      */
     public Integer[] deleteEvens(Integer[] ints) {
+        String temp = "";
+        for (int i = 0; i < ints.length; i++){
+   //         if (ints[i] %2 !=0) {
+                temp += ints[i];
+   //         }
+        }
+        System.out.println(temp);
         return null;
     }
+
+
+
 
     /**
      * @param ints array of Integer objects
