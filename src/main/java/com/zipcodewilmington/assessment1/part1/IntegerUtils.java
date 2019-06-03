eb43647a3d45aa3186deae43522daf5ab5b3d9d3
donnasvinis <dvsvinis@mac.com>
2019-06-03 10:17:46 -0400

    Completed Part 1 IntegerUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.assessment1.part1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -9,9 +13,15 @@ public class IntegerUtils {
     /**
      * @param n integer value input by client
      * @return the sum of all integers between 0 and not including `n`
+     * The way this question reads I think it should be 0+1+2+3+4 = 10 (not including 5)
+     * but that doesn't pass the test
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        Integer sumOfN = 0;
+        for (int i = 1; i <= n; i++) {
+            sumOfN = sumOfN + i;
+        }
+        return sumOfN;
     }
 
     /**
@@ -19,7 +29,11 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+        Integer productOfN = 1;
+        for (int i = 1; i <= n; i++) {
+            productOfN = productOfN * i;
+        }
+        return productOfN;
     }
 
     /**
@@ -27,6 +41,10 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+        String str = val.toString();
+        String reverse = "";
+        for(int i = str. length() - 1; i >= 0; i--) {
+            reverse = reverse + str. charAt(i); }
+        return Integer.valueOf(reverse);
     }
 }
