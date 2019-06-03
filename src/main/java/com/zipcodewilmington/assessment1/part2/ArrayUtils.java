d8d9c9953ebb5dd2163ff3ba81366d5924162923
donnasvinis <dvsvinis@mac.com>
2019-06-03 12:24:15 -0400

    Partly through Part 2 ArrayUtils

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.*;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -11,18 +13,39 @@ public class ArrayUtils {
      * Given an array of objects, named `objectArray`, and an object `objectToCount`, return the number of times the `objectToCount` appears in the `objectArray`
      */
     public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
-        return null;
+        Integer counter = 0;
+
+        for (int i = 0; i < objectArray.length; i++){
+            if (objectArray[i].equals(objectToCount)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    /**
+
+
+    /**  NOT WORKING
      * @param objectArray    an array of any type of Object
      * @param objectToRemove a value to be removed from the `objectArray`
      * @return an array with identical content excluding the specified `objectToRemove`
      * Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
      */
     public static Object[] removeValue(Object[] objectArray, Object objectToRemove) {
-        return null;
+
+//        ArrayList<Object> arrayList = new ArrayList<Object>(Arrays.asList(objectArray));
+//        System.out.println(arrayList);
+//
+//        for (int i = 0; i < objectArray.length; i++){
+//            if (arrayList[i] == objectArray){
+//                arrayList.remove(i);
+//            }
+//        }
+//
+       return null;
     }
+
+
 
     /**
      * @param objectArray an array of any type of Object
@@ -30,7 +53,29 @@ public class ArrayUtils {
      * given an array of objects, named `objectArray` return the most frequently occuring object in the array
      */
     public static Object getMostCommon(Object[] objectArray) {
-        return null;
+        Arrays.sort(objectArray);
+        int counter = 0;
+        Object curr = objectArray[0];
+        Object maxvalue;
+        int maxcounter = -1;
+        maxvalue = objectArray[0];
+
+        for (Object each : objectArray){
+            if (curr == each){
+                counter++;
+            } else {
+                if (counter > maxcounter){
+                    maxcounter = counter;
+                    maxvalue = curr;
+                }
+                counter = 0;
+                curr = each;
+            }
+        }
+        if (counter > maxcounter){
+            maxvalue = curr;
+        }
+    return maxvalue;
     }
 
 
@@ -40,6 +85,9 @@ public class ArrayUtils {
      * given an array of objects, named `objectArray` return the least frequently occuring object in the array
      */
     public static Object getLeastCommon(Object[] objectArray) {
+
+
+
         return null;
     }
 
