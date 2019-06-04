62f9e0036b3aad46eca75fb0c72fef703dc8a84e
donnasvinis <dvsvinis@mac.com>
2019-06-03 20:37:39 -0400

    Part2 multiplesDelete Test1 still no tests passing

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -23,13 +23,12 @@ public class MultiplesDeleter {
             }
         }
         System.out.println(("list " + list.toString()));
-        String output = list.toString();
 
         //Convert to object array
 //        Object[] array = list.toArray();
 //        System.out.println( Arrays.toString(array) );
 
-        return null;
+        return null;     //??? HOW DO I TURN THE ARRAYLIST INTO AN INTEGER[]  ???
     }
 
 
@@ -41,8 +40,19 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with odds removed
      */
     public Integer[] deleteOdds(Integer[] ints) {
+        List<Integer> evens = new ArrayList<Integer>();
+        for(int i = 0; i < ints.length; i++) {
+            if(ints[i] % 2 == 0)
+            {
+                evens.add(ints[i]);
+            }
+        }
+        System.out.println(("Evens " + evens.toString()));
         return null;
-    }
+   }
+
+
+
 
     /**
      * @param ints array of Integer objects
@@ -50,6 +60,14 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public Integer[] deleteMultiplesOf3(Integer[] ints) {
+        List<Integer> threes = new ArrayList<Integer>();
+        for(int i = 0; i < ints.length; i++) {
+            if(ints[i] % 3 != 0)
+            {
+                threes.add(ints[i]);
+            }
+        }
+        System.out.println(("Threes " + threes.toString()));
         return null;
     }
 
@@ -60,6 +78,15 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
+        List<Integer> multipleN = new ArrayList<Integer>();
+        for(int i = 0; i < ints.length; i++) {
+            if(ints[i] % multiple != 0)
+            {
+                multipleN.add(ints[i]);
+            }
+        }
+        System.out.println(("multipleN " + multipleN.toString()));
         return null;
     }
+
 }
